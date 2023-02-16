1725ddb345ee47235adf4cdf77a3a72866300251
João <joaomanoeldev@gmail.com>
2023-02-15 23:03:01 -0300

    concat array

---
+++ concatArray/index.js
@@ -0,0 +1,8 @@
+const array1 = [1, 2, 3]
+const array2 = [4, 5, 6]
+
+// const array3 = array1.concat(array2, [7, 8, 9], 'João', 'Karolaine')
+
+const array = [...array1, ...array2, ...[7, 8, 9], "João"]
+ 
+console.log(array)
